6b345ece11f3e1fff4d359ef5110eabe1facd949
Hannes Widmoser <widmoser@gmail.com>
2025-06-05 22:56:48 +0200

    Redirect to agent dep.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello, World!");
+import { getInput } from '@actions/core';
+import { createPullRequestForIssue } from '@widmoser/agent';
+
+const repository = getInput('repository');
+const branch = getInput('branch');
+const apiKey = getInput('openrouter_api_key');
+const model = getInput('model');
+const githubToken = getInput('github_token');
+
+await createPullRequestForIssue(repository, branch, apiKey, model, githubToken);
+
+export {};
